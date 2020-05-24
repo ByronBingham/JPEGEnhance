d8863c74f4fa44f6e8cb8bd55007b46dc414f541
Byron Bingham <bybingham@gmail.com>
2020-05-23 18:53:05 -0600

    Added multiprocessing to the dataset batch creation process

--- JPGPNGNetwork/JPGEnhance.py
+++ JPGPNGNetwork/JPGEnhance.py
@@ -1,217 +1,190 @@
 # import os
 import tensorflow as tf
-from modules.Dataset import DataSet as ds
-from modules.Dataset import WINDOW_SIZE
+from modules.Dataset import DataSet, WINDOW_SIZE
 
 import numpy as np
 import datetime as dt
 
-# os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-# set directory to save NN's data to
+class JPGEnhance:
 
-# get directory for training data
+    def __init__(self):
+        self.model = None
+        self.checkpointPath = None
+        self.dataSet = DataSet()
 
-# tf.debugging.set_log_device_placement(True)
+    def main(self):
 
-tf.compat.v1.enable_control_flow_v2()
-print("eager or not: " + str(tf.executing_eagerly()))
+        tf.compat.v1.enable_control_flow_v2()
+        print("eager or not: " + str(tf.executing_eagerly()))
 
-# create network
-print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
-print(tf.config.experimental.list_physical_devices('GPU'))
+        # create network
+        print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+        print(tf.config.experimental.list_physical_devices('GPU'))
 
-physical_devices = tf.config.experimental.list_physical_devices('GPU')
-
-try:
-    tf.config.experimental.set_memory_growth(physical_devices[0], True)
-except Exception as e:
-    print(e)
-
-inputs = tf.keras.Input(shape=(WINDOW_SIZE, WINDOW_SIZE, 3))
-
-conv1 = tf.keras.layers.Conv2D(filters=32, kernel_size=(4, 4), strides=2)(inputs)
-conv2 = tf.keras.layers.Conv2D(filters=32, kernel_size=(2, 2), strides=1)(conv1)
-conv3 = tf.keras.layers.Conv2D(filters=32, kernel_size=(2, 2), strides=1)(conv2)
-
-flatConv = tf.keras.layers.Flatten()(conv3)
-flatInput = tf.keras.layers.Flatten()(inputs)
-
-inputAndConv = tf.keras.layers.Concatenate()([flatConv, flatInput])
-
-dense1 = tf.keras.layers.Dense(units=WINDOW_SIZE * WINDOW_SIZE * 3, input_shape=(WINDOW_SIZE, WINDOW_SIZE, 3),
-                               use_bias=True)(inputAndConv)
-
-dense2 = tf.keras.layers.Dense(units=WINDOW_SIZE * WINDOW_SIZE * 3, input_shape=(WINDOW_SIZE, WINDOW_SIZE, 3),
-                               use_bias=True)(dense1)
-
-out = tf.keras.layers.Reshape(target_shape=(WINDOW_SIZE, WINDOW_SIZE, 3))(dense2)
-
-model = tf.keras.Model(inputs=inputs, outputs=out)
-
-model.compile(loss=tf.keras.losses.MeanSquaredError(), optimizer='adam', metrics=['accuracy'])
-model.summary()
-
-tf.keras.utils.plot_model(model, to_file='model.png')
-
-# load model checkpoint if exists
-checkpointPath = "./JPGEnhanceCheckpoint_" + str(WINDOW_SIZE)
-
-try:
-    model.load_weights(checkpointPath)
-except Exception as e:
-    print("Weights not loaded. Will create new weights")
-    print(str(e))
-
-# load data set
-dataset = ds()
-print("Finished initializing dataset")
-
-
-# start training
-
-def trainStep():
-    trainData, targetData, originalSize = dataset.makeNextBatch()
-    print("finished creating batch")
-    print("Batch size: " + str(len(targetData)))
-    counter = 0
-
-    trainData = np.asarray(trainData)
-    targetData = np.asarray(targetData)
-
-    for i in range(len(trainData)):
-        time1 = dt.datetime.now()
-        with tf.device('/GPU:0'):
-            model.fit(trainData[i], targetData, verbose=2, epochs=1, batch_size=4)
-        print("Time elapsed training: " + str(dt.datetime.now() - time1))
-
-    model.save_weights(checkpointPath)
-
-
-def clipData(data):
-    for i in range(0, len(data)):
-
-        for w in range(0, WINDOW_SIZE):
-            for h in range(0, WINDOW_SIZE):
-                for p in range(0, 3):
-                    if data[i][w][h][p] > 255.:
-                        data[i][w][h][p] = 255.
-                    if data[i][w][h][p] < 0.:
-                        data[i][w][h][p] = 0.
-
-    return data
-
-
-def getEvalCounter():
-    # get number of the evaluation
-    number = 0
-
-    evalCounterFilename = "./evalCounter_" + str(WINDOW_SIZE) + ".cfg"
-
-    # open file
-    file = None
-    try:
-        file = open(evalCounterFilename, "r")
-    except Exception as e:
-        print("Could not open counter file. Initializing new file...")
-        print(str(e))
+        physical_devices = tf.config.experimental.list_physical_devices('GPU')
 
         try:
-            file = open(evalCounterFilename, "x")
-            file.close()
-            file = open(evalCounterFilename, "w")
-            file.write("0")
-            file.close()
-            file = open(evalCounterFilename, "r")
+            tf.config.experimental.set_memory_growth(physical_devices[0], True)
         except Exception as e:
-            print("Probably a filesystem error")
             print(e)
 
-    # read from file
-    value = int(file.readline())
-    file.close()
+        inputs = tf.keras.Input(shape=(WINDOW_SIZE, WINDOW_SIZE, 3))
 
-    # update count
-    try:
-        file = open(evalCounterFilename, "w")
-        file.write(str(value + 1))
-        file.close()
-    except Exception as e:
-        print("Could not update evaluation counter")
-        print(str(e))
+        conv1 = tf.keras.layers.Conv2D(filters=32, kernel_size=(4, 4), strides=2)(inputs)
+        conv2 = tf.keras.layers.Conv2D(filters=32, kernel_size=(2, 2), strides=1)(conv1)
+        conv3 = tf.keras.layers.Conv2D(filters=32, kernel_size=(2, 2), strides=1)(conv2)
 
-    return value
+        flatConv = tf.keras.layers.Flatten()(conv3)
+        flatInput = tf.keras.layers.Flatten()(inputs)
 
+        inputAndConv = tf.keras.layers.Concatenate()([flatConv, flatInput])
 
-def evalStep():
-    trainData, targetData, originalSize = dataset.makeNextBatch()
+        dense1 = tf.keras.layers.Dense(units=WINDOW_SIZE * WINDOW_SIZE * 3,
+                                       input_shape=(WINDOW_SIZE, WINDOW_SIZE, 3),
+                                       use_bias=True)(inputAndConv)
 
-    trainData = np.asarray(trainData)
-    targetData = np.asarray(targetData)
+        dense2 = tf.keras.layers.Dense(units=WINDOW_SIZE * WINDOW_SIZE * 3,
+                                       input_shape=(WINDOW_SIZE, WINDOW_SIZE, 3),
+                                       use_bias=True)(dense1)
 
-    print("finished creating batch")
-    print("Batch size: " + str(len(targetData)))
-    inputImg = trainData[len(trainData) - 1]
-    prediction = model.predict(inputImg)
+        out = tf.keras.layers.Reshape(target_shape=(WINDOW_SIZE, WINDOW_SIZE, 3))(dense2)
 
-    # save output
-    number = getEvalCounter()
-    prediction = clipData(prediction)
-    prediction = np.array(prediction).astype(dtype=np.uint8)
-    dataset.saveResults(targetData, inputImg, prediction, originalSize, number)
-    print("Evaluation step finished\n")
+        self.model = tf.keras.Model(inputs=inputs, outputs=out)
 
+        self.model.compile(loss=tf.keras.losses.MeanSquaredError(), optimizer='adam', metrics=['accuracy'])
+        self.model.summary()
 
-imagesToTrain = 100000
-evalFrequency = 25
-i = 0
-while True:  # i < imagesToTrain:
-    try:
-        trainStep()
-    except Exception as e:
-        print("Training step failed. Skipping step")
-        print(str(e) + "\n")
-        continue
+        tf.keras.utils.plot_model(self.model, to_file='model.png')
 
-    accuracy = model.metrics
-    print("Training step " + str(i) + " finished\n")
+        # load model checkpoint if exists
+        self.checkpointPath = "./JPGEnhanceCheckpoint_" + str(WINDOW_SIZE)
 
-    if i % evalFrequency == 0:
-        print("Beginning eval step")
         try:
-            evalStep()
+            self.model.load_weights(self.checkpointPath)
         except Exception as e:
-            print("Evaluation step failed. Skipping step")
-            print(str(e) + "\n")
-            continue
+            print("Weights not loaded. Will create new weights")
+            print(str(e))
 
-    i += 1
+        # load data set
+        self.dataSet.startFillQueue()
+        print("Finished initializing dataset")
 
-'''
-def train2():
-    trainData, targetData = dataset.makeNextBatch()
-    print("Finished creating batch")
-    fTarget = np.array(targetData).astype(dtype=np.float)
+        # start training
 
-    for i in range(0, len(trainData)):
-        with tf.GradientTape() as tape:
-            predictions = model.predict(trainData[i], batch_size=len(targetData))
-            # predictions = predictions.astype(dtype=np.uint8)
-            loss = loss_object(fTarget, predictions)
+        imagesToTrain = 100000
+        evalFrequency = 25
+        i = 0
+        while True:  # i < imagesToTrain:
+            try:
+                self.trainStep()
+            except Exception as e:
+                print("Training step failed. Skipping step")
+                print(str(e) + "\n")
+                continue
 
-        print("Loss: " + str(loss.numpy()))
-        gradients = tape.gradient(loss, model.trainable_variables)
-        optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+            accuracy = self.model.metrics
+            print("Training step " + str(i) + " finished\n")
+
+            if i % evalFrequency == 0:
+                print("Beginning eval step")
+                try:
+                    self.evalStep()
+                except Exception as e:
+                    print("Evaluation step failed. Skipping step")
+                    print(str(e) + "\n")
+                    continue
+
+            i += 1
+
+        DataSet.destroy()
+
+    def clipData(self, data):
+        for i in range(0, len(data)):
+
+            for w in range(0, WINDOW_SIZE):
+                for h in range(0, WINDOW_SIZE):
+                    for p in range(0, 3):
+                        if data[i][w][h][p] > 255.:
+                            data[i][w][h][p] = 255.
+                        if data[i][w][h][p] < 0.:
+                            data[i][w][h][p] = 0.
+
+        return data
+
+    def getEvalCounter(self):
+        # get number of the evaluation
+        number = 0
+
+        evalCounterFilename = "./evalCounter_" + str(WINDOW_SIZE) + ".cfg"
+
+        # open file
+        file = None
+        try:
+            file = open(evalCounterFilename, "r")
+        except Exception as e:
+            print("Could not open counter file. Initializing new file...")
+            print(str(e))
+
+            try:
+                file = open(evalCounterFilename, "x")
+                file.close()
+                file = open(evalCounterFilename, "w")
+                file.write("0")
+                file.close()
+                file = open(evalCounterFilename, "r")
+            except Exception as e:
+                print("Probably a filesystem error")
+                print(e)
+
+        # read from file
+        value = int(file.readline())
+        file.close()
+
+        # update count
+        try:
+            file = open(evalCounterFilename, "w")
+            file.write(str(value + 1))
+            file.close()
+        except Exception as e:
+            print("Could not update evaluation counter")
+            print(str(e))
+
+        return value
+
+    def evalStep(self):
+        trainData, targetData, originalSize = self.dataSet.getBatch()
+
+        trainData = np.asarray(trainData)
+        targetData = np.asarray(targetData)
+
+        inputImg = trainData[len(trainData) - 1]
+        prediction = self.model.predict(inputImg)
+
+        # save output
+        number = self.getEvalCounter()
+        prediction = self.clipData(prediction)
+        prediction = np.array(prediction).astype(dtype=np.uint8)
+        DataSet.saveResults(targetData, inputImg, prediction, originalSize, number)
+        print("Evaluation step finished\n")
+
+    def trainStep(self):
+        trainData, targetData, originalSize = self.dataSet.getBatch()
+        counter = 0
+
+        trainData = np.asarray(trainData)
+        targetData = np.asarray(targetData)
+
+        for i in range(len(trainData)):
+            time1 = dt.datetime.now()
+            with tf.device('/GPU:0'):
+                self.model.fit(trainData[i], targetData, verbose=2, epochs=1, batch_size=4)
+            print("Time elapsed training: " + str(dt.datetime.now() - time1))
+
+        self.model.save_weights(self.checkpointPath)
+        print("Done with training step")
 
 
-def testAccuracy():
-    trainData, targetData = dataset.makeNextBatch()
-    print("Finished creating batch")
-
-    for i in range(0, len(trainData)):
-        predictions = model.predict(trainData[i], batch_size=len(targetData))
-        loss = loss_object(targetData, predictions)
-
-        testLoss(loss)
-        return accuracyObj(targetData, predictions)
-'''
+if __name__ == '__main__':
+    enhance = JPGEnhance()
+    enhance.main()
--- JPGPNGNetwork/modules/Dataset.py
+++ JPGPNGNetwork/modules/Dataset.py
@@ -4,30 +4,145 @@ from random import randrange
 from PIL import Image
 from os.path import join
 import numpy as np
+import time
+import multiprocessing
 
-size = input("Enter window size for the network: \n")
-WINDOW_SIZE = int(size)
-print("Window size: " + str(WINDOW_SIZE))
+WINDOW_SIZE = 32
+DATASET_BASE_DIR = "C:\\ImgDataset"
 
 
 class DataSet:
+    processes = []
+
+    NUMBER_OF_CPU_THREADS = 20
+    SIZE_OF_QUEUE = 15
 
     def __init__(self):
-        self.datasetBaseDir = "C:\\ImgDataset"  # input("Input base directory for the dataset:\n")
-        if not os.path.exists(self.datasetBaseDir):
-            print("Invalid path\n")
-            self.datasetBaseDir = input("Input base directory for the dataset:\n")
-        self.baseDir = self.datasetBaseDir
-        self.datasetDir = [f for f in os.listdir(self.datasetBaseDir)
-                           if os.path.isdir(os.path.join(self.datasetBaseDir, f))]
+        self.datasetDir = [f for f in os.listdir(DATASET_BASE_DIR)
+                           if os.path.isdir(os.path.join(DATASET_BASE_DIR, f))]
         self.datasetSize = len(self.datasetDir)
+        self.batchQueue = multiprocessing.Queue()
+        self.currentlyProcessing = multiprocessing.Value('i', 0)
 
-    def getImageSet(self, index):
-        dir = os.path.join(self.baseDir, self.datasetDir[index])
+    def getBatch(self):
+        if not self.batchQueue.empty():
+            batch = self.batchQueue.get()
+            print("Batch Available\n"
+                  "Batch size: " + str(len(batch[1])) + "\n"
+                  "Size of queue: " + str(self.batchQueue.qsize() - 1))
+            return batch
+        else:
+            time.sleep(1)
+            return self.getBatch()
+
+    @classmethod
+    def destroy(cls):
+        print("Waiting for threads to end...")
+        for p in cls.processes:
+            p.join()
+
+    def startFillQueue(self):
+        for x in range(0, DataSet.NUMBER_OF_CPU_THREADS):
+            self.processes.append(
+                multiprocessing.Process(target=DataSetProcessor.asyncUpdateDataset,
+                                        args=(self.batchQueue, self, self.currentlyProcessing)))
+
+        i = 0
+        for p in self.processes:
+            print("Starting process" + str(i))
+            p.start()
+            i += 1
+
+    @classmethod
+    def saveResults(cls, target, inputImage, predictions, originalShape, iterarion):
+        if len(target) != len(predictions):
+            raise IndexError("Prediction's dimensions do not match the Target")
+        if len(originalShape) != 2:
+            raise IndexError("Invalid original shape")
+
+        width, height = originalShape
+
+        tPixels = cls.reassemble(target, width, height)
+        iPixels = cls.reassemble(inputImage, width, height)
+        pPixels = cls.reassemble(predictions, width, height)
+
+        tPixels = tPixels.astype('uint8')
+        iPixels = iPixels.astype('uint8')
+        pPixels = pPixels.astype('uint8')
+
+        tImage = pil.Image.fromarray(tPixels, "RGB")
+        iImage = pil.Image.fromarray(iPixels, "RGB")
+        pImage = pil.Image.fromarray(pPixels, "RGB")
+
+        tImage = tImage.crop((0, 0, width, height))
+        iImage = iImage.crop((0, 0, width, height))
+        pImage = pImage.crop((0, 0, width, height))
+
+        # create folder for images
+        resDir = "./Results_" + str(WINDOW_SIZE)
+        if not os.path.exists(resDir):
+            os.mkdir(resDir)
+
+        resDir = "./Results_" + str(WINDOW_SIZE) + "/Eval_" + str(iterarion)
+        os.mkdir(resDir)
+
+        # save images to new folder
+        tImage.save(resDir + "/original.png", format="PNG")
+        iImage.save(resDir + "/compressed.png", format="PNG")
+        pImage.save(resDir + "/NNOut.png", format="PNG")
+
+    @classmethod
+    def reassemble(cls, pixels, width, height):
+
+        partW = int(width / WINDOW_SIZE)
+        if (width % WINDOW_SIZE) > 0:
+            partW += 1
+        partH = int(height / WINDOW_SIZE)
+        if height % WINDOW_SIZE > 0:
+            partH += 1
+
+        image = [[[0] * 3] * partW * WINDOW_SIZE] * partH * WINDOW_SIZE
+        image = np.array(image)
+        for w in range(0, partW):
+            for h in range(0, partH):
+
+                for y in range(0, WINDOW_SIZE):
+                    for x in range(0, WINDOW_SIZE):
+                        image[h * WINDOW_SIZE + y][w * WINDOW_SIZE + x] = pixels[w * partH + h][x][y]
+
+        return image
+
+
+class DataSetProcessor:
+    queue = None
+    dataSet = None
+    currentlyProcessing = None
+
+    @classmethod
+    def asyncUpdateDataset(cls, queue, dataSet, currentlyProcessing):
+        cls.queue = queue
+        cls.dataSet = dataSet
+        cls.currentlyProcessing = currentlyProcessing
+
+        if queue.qsize() + currentlyProcessing.value < DataSet.SIZE_OF_QUEUE:
+            currentlyProcessing.value += 1
+
+            print("Creating batch async")
+            cls.makeNextBatch()
+            print("Thread finished creating batch")
+
+            currentlyProcessing.value -= 1
+        else:
+            time.sleep(1)
+
+    @classmethod
+    def getImageSet(cls, index):
+        dir = os.path.join(DATASET_BASE_DIR, cls.dataSet.datasetDir[index])
         images = [f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir, f)) and f.endswith(('.png', '.jpg'))]
         return images
 
-    def getTargetImage(self, images):
+    @classmethod
+    def getTargetImage(cls, images):
         target = [f for f in images if f.endswith('.png')]
         try:
             return target[0]
@@ -37,7 +152,8 @@ class DataSet:
 
         return None
 
-    def getTrainingImages(self, images):
+    @classmethod
+    def getTrainingImages(cls, images):
         training = [f for f in images if f.endswith('.jpg')]
         try:
             return training
@@ -47,7 +163,8 @@ class DataSet:
 
         return None
 
-    def partitionImage(self, image: pil.Image):
+    @classmethod
+    def partitionImage(cls, image: pil.Image):
         pixels = np.asarray(image)
         pixels = pixels.astype('uint8')
 
@@ -81,72 +198,17 @@ class DataSet:
 
         return batches
 
-    def makeNextBatch(self):
-        index = randrange(0, self.datasetSize)
-        images = self.getImageSet(index)
-        target = Image.open(join(self.baseDir, self.datasetDir[index], self.getTargetImage(images)))
-        training = [Image.open(join(self.baseDir, self.datasetDir[index], f)) for f in self.getTrainingImages(images)]
+    @classmethod
+    def makeNextBatch(cls):
+        index = randrange(0, cls.dataSet.datasetSize)
+        images = cls.getImageSet(index)
+        target = Image.open(join(DATASET_BASE_DIR, cls.dataSet.datasetDir[index], cls.getTargetImage(images)))
+        training = [Image.open(join(DATASET_BASE_DIR, cls.dataSet.datasetDir[index], f)) for f in
+                    cls.getTrainingImages(images)]
 
         trainingData = []
         for dat in training:
-            trainingData.append(self.partitionImage(dat))
-        labeledData = self.partitionImage(target)
+            trainingData.append(cls.partitionImage(dat))
+        labeledData = cls.partitionImage(target)
 
-        return trainingData, labeledData, target.size
-
-    def reassemble(self, pixels, width, height):
-
-        partW = int(width / WINDOW_SIZE)
-        if (width % WINDOW_SIZE) > 0:
-            partW += 1
-        partH = int(height / WINDOW_SIZE)
-        if height % WINDOW_SIZE > 0:
-            partH += 1
-
-        image = [[[0] * 3] * partW * WINDOW_SIZE] * partH * WINDOW_SIZE
-        image = np.array(image)
-        for w in range(0, partW):
-            for h in range(0, partH):
-
-                for y in range(0, WINDOW_SIZE):
-                    for x in range(0, WINDOW_SIZE):
-                        image[h * WINDOW_SIZE + y][w * WINDOW_SIZE + x] = pixels[w * partH + h][x][y]
-
-        return image
-
-    def saveResults(self, target, inputImage, predictions, originalShape, iterarion):
-        if len(target) != len(predictions):
-            raise IndexError("Prediction's dimensions do not match the Target")
-        if len(originalShape) != 2:
-            raise IndexError("Invalid original shape")
-
-        width, height = originalShape
-
-        tPixels = self.reassemble(target, width, height)
-        iPixels = self.reassemble(inputImage, width, height)
-        pPixels = self.reassemble(predictions, width, height)
-
-        tPixels = tPixels.astype('uint8')
-        iPixels = iPixels.astype('uint8')
-        pPixels = pPixels.astype('uint8')
-
-        tImage = pil.Image.fromarray(tPixels, "RGB")
-        iImage = pil.Image.fromarray(iPixels, "RGB")
-        pImage = pil.Image.fromarray(pPixels, "RGB")
-
-        tImage = tImage.crop((0, 0, width, height))
-        iImage = iImage.crop((0, 0, width, height))
-        pImage = pImage.crop((0, 0, width, height))
-
-        # create folder for images
-        resDir = "./Results_" + str(WINDOW_SIZE)
-        if not os.path.exists(resDir):
-            os.mkdir(resDir)
-
-        resDir = "./Results_" + str(WINDOW_SIZE) + "/Eval_" + str(iterarion)
-        os.mkdir(resDir)
-
-        # save images to new folder
-        tImage.save(resDir + "/original.png", format="PNG")
-        iImage.save(resDir + "/compressed.png", format="PNG")
-        pImage.save(resDir + "/NNOut.png", format="PNG")
+        cls.queue.put((trainingData, labeledData, target.size))
